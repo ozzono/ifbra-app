d2f0a187d3db53ee30bd2e3b30b233b2da9fd2ac
Hugo Virgílio <huvirgilio@gmail.com>
2020-02-07 00:24:13 -0200

    Initial commit... again

---
+++ src/components/ApiTest2.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import axios from "axios";
+const baseDomain = "localhost:8000";
+const baseUrl = `${baseDomain}/api`;
+export default axios.create({
+  baseUrl
+});
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/PokeApi.vue
@@ -0,0 +1,37 @@
+<template>
+  <Page class="page">
+    <ActionBar class="action-bar" title="Pokemon"></ActionBar>
+    <GridLayout>
+      <ListView for="p in pokemon" class="list-group">
+        <v-template>
+          <StackLayout class="list-group-item">
+            <Label :text="p.name" />
+          </StackLayout>
+        </v-template>
+      </ListView>
+    </GridLayout>
+  </Page>
+</template>
+
+<script>
+import * as http from "http";
+export default {
+  data() {
+    return {
+      pokemon: []
+    };
+  },
+  mounted() {
+    http
+      .getJSON("https://pokeapi.co/api/v2/pokemon/?limit=151")
+      .then(result => {
+        this.pokemon = result.results;
+        //   },
+        //   error => {
+        //     console.log(error);
+      });
+  }
+};
+</script>
+
+<style scoped></style>
