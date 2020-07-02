30da10844f605576573b05e945e06b620f3bbc58
Hugo Virgílio <huvirgilio@gmail.com>
2020-07-02 18:04:19 -0300

    Adding inheritance from parent to child component

---
+++ src/components/test/Child.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>text: {{message}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"Child",
+  props:{
+    message: String
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/test/Parent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <Child message="test"></Child>
+  </div>
+</template>
+
+<script>
+import Child from "@/components/test/Child";
+export default {
+    name:"Parent",
+    components:{
+        Child
+    }
+}
+</script>
+
+<style>
+
+</style>
