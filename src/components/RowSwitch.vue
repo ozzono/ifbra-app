fff19b1662ad2ac0ffe956afc44074c4c55251af
Hugo Virgílio <huvirgilio@gmail.com>
2020-07-02 19:35:36 -0300

    Form2 v0.1 finished

--- src/components/RowSwitch.vue
+++ src/components/RowSwitch.vue
@@ -1,10 +1,9 @@
 <template>
   <v-container fluid>
-    <v-switch v-model="switch1" :label="`Switch 1: ${switch1.toString()}`"></v-switch>
-    <v-switch v-model="switch2" :label="`Switch 2: ${switch2.toString()}`"></v-switch>
+    <!-- <v-switch v-model="switch1"></v-switch> -->
+    <v-switch></v-switch>
   </v-container>
 </template>
-
 
 <script>
   export default {
--- src/components/forms/Form2.vue
+++ src/components/forms/Form2.vue
@@ -1,20 +1,35 @@
 <template>
   <div class="form">
-    <v-container class="grey lighten-5">
-      <div v-for="(funcao,i) in funcoes[0]" :key=i>
-        {{i+1}}. {{funcao.Tipo}}
-        <v-row v-for="(subfuncao,j) in funcao.SubFuncao" :key=j>{{i+1}}.{{j+1}} {{subfuncao.Tipo}}: {{subfuncao.Detalhe}}</v-row>          
-        <hr>
-      </div>
-    </v-container>
+    <v-banner>
+      <h5>Formulário 2</h5>
+      <h6>Funções corporais acometidas <i>(a ser preenchido pelo médico perito)</i></h6>
+      <v-container class="grey lighten-5">
+        <div v-for="(funcao,i) in funcoes[0]" :key=i>
+          {{i+1}}. {{funcao.Tipo}}
+          <div class="d-flex" no-gutters v-for="(subfuncao,j) in funcao.SubFuncao" :key=j>
+            <div cols="pa-1">
+              <RowSwitch>Rowswitch</RowSwitch>
+            </div>
+            <div cols="pa-11" class="align-self-center">
+              {{i+1}}.{{j+1}} {{subfuncao.Tipo}}: {{subfuncao.Detalhe}}
+            </div>
+          </div>
+          <hr>
+        </div>
+      </v-container>
+    </v-banner>
   </div>
 </template>
 <script>
 import Funcoes from "@/assets/json/form2.json";
+import RowSwitch from "@/components/RowSwitch";
 export default {
-data: () => ({
+    data: () => ({
       funcoes: Object.values(Funcoes)
-    })
+    }),
+    components:{
+      RowSwitch:RowSwitch
+    }
 }
 </script>
 
