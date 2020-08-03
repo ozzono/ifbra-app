4986b16f6d858c795cecc9629468bcc3f7d539e1
Hugo Virgílio <huvirgilio@gmail.com>
2020-08-03 03:16:03 -0300

    More unused dependencies removed

--- src/main.js
+++ src/main.js
@@ -6,8 +6,6 @@ import "./plugins/bootstrap-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
 import vuetify from "./plugins/vuetify";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@mdi/font/css/materialdesignicons.css";
