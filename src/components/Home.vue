904d36962be20a9b06f71473f87e858f138a34f8
Hugo Virgílio <huvirgilio@gmail.com>
2020-08-04 12:03:33 -0300

    Dependencies update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <Evaluation />
+    <!-- <Evaluation /> -->
     <!-- <Form1 /> -->
     <!-- <Form2 /> -->
-    <!-- <Form3 /> -->
+    <Form3 />
     <!-- <Form4 /> -->
   </div>
 </template>
@@ -11,10 +11,10 @@
 <script>
 export default {
   components: {
-    Evaluation: () => import("@/components/Evaluation")
+    // Evaluation: () => import("@/components/Evaluation")
     // Form1: () => import("@/components/forms/Form1"),
     // Form2: () => import("@/components/forms/Form2"),
-    // Form3: () => import("@/components/forms/Form3"),
+    Form3: () => import("@/components/forms/Form3")
     // Form4: () => import("@/components/forms/Form4")
   }
 };
