8407bf70599a25a824bce9bc18d859f0a13ea5a5
Hugo Virgílio <huvirgilio@gmail.com>
2020-08-11 14:34:36 -0300

    npm update && npm audit-fix

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,23 +1,23 @@
 <template>
   <div>
-    <!-- <Evaluation /> -->
+    <Evaluation />
     <Report />
-    <!-- <Form1 /> -->
-    <!-- <Form2 /> -->
+    <Form1 />
+    <Form2 />
     <Form3 />
-    <!-- <Form4 /> -->
+    <Form4 />
   </div>
 </template>
 
 <script>
 export default {
   components: {
-    // Evaluation: () => import("@/components/Evaluation"),
+    Evaluation: () => import("@/components/Evaluation"),
     Report: () => import("@/components/Report"),
-    // Form1: () => import("@/components/forms/Form1"),
-    // Form2: () => import("@/components/forms/Form2"),
+    Form1: () => import("@/components/forms/Form1"),
+    Form2: () => import("@/components/forms/Form2"),
     Form3: () => import("@/components/forms/Form3"),
-    // Form4: () => import("@/components/forms/Form4")
+    Form4: () => import("@/components/forms/Form4")
   }
 };
 </script>
