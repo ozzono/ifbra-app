9b87a873c5ac15ad876a6cafad5a28bce9ef8dc9
Hugo Virgílio <huvirgilio@gmail.com>
2020-08-12 11:04:22 -0300

    I need only to vertically realign

--- src/components/forms/Form4.vue
+++ src/components/forms/Form4.vue
@@ -7,45 +7,68 @@
           subtitle="Aplicação do Modelo Linguístico Fuzzy"
           comment=""
         />
-        <v-row class="d-flex">
-          <v-col :cols="2">
+        <v-row class="flex">
+          <v-col :cols="12">
             Domínios e Atividades
           </v-col>
         </v-row>
-        <v-row dense v-for="(deficiecia, i) in Deficiencias" :key="i">
-          <v-col cols="2">
-            {{ deficiecia.Desc }}
-          </v-col>
-          <v-col cols="1">
-            <RowSwitch></RowSwitch>
-          </v-col>
-          <v-col>
-            {{ deficiecia.Obs }}
-          </v-col>
-          <v-col cols="1">
-            <RowSwitch></RowSwitch>
-          </v-col>
-          <v-col>
-            Houve pontuação 25,50 ou 75 em alguma atividade do Domínio
-            {{ deficiecia.Dominios }}
-          </v-col>
-        </v-row>
+        <div v-for="(deficiecia, i) in Deficiencias" :key="i">
+          <v-divider v-if="i > 0" class="hidden-md-and-up" :inset="true" />
+          <v-row class="align-center">
+            <v-col class="text-center" md="2" cols="12">
+              {{ deficiecia.Desc }}
+            </v-col>
+            <v-col cols="10">
+              <v-row>
+                <v-col cols="12" md="4">
+                  <v-row class="align-center">
+                    <v-col>
+                      <RowSwitch :innerLabel="deficiecia.Obs" />
+                    </v-col>
+                  </v-row>
+                </v-col>
+                <v-col cols="12" md="4">
+                  <v-row class="align-center">
+                    <v-col cols="12">
+                      <RowSwitch
+                        :innerLabel="
+                          `Houve pontuação 25 ou 50 em alguma atividade dos domínios ${deficiecia.Dominios}`
+                        "
+                      />
+                    </v-col>
+                    <v-col> </v-col>
+                  </v-row>
+                </v-col>
+                <v-col cols="12" md="4">
+                  <v-row class="align-center">
+                    <v-col cols="12">
+                      <RowSwitch
+                        :innerLabel="
+                          `Houve pontuação 75 em todas as atividade dos domínios ${deficiecia.Dominios}`
+                        "
+                      />
+                    </v-col>
+                  </v-row>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+        </div>
       </v-container>
     </v-flex>
   </div>
 </template>
 
 <script>
-import FormHeader from "@/components/forms/FormHeader";
-import RowSwitch from "@/components/RowSwitch";
+/* eslint-disable no-console */
 import Deficiencias from "@/assets/json/form4.json";
 export default {
   data: () => ({
     Deficiencias: Object.values(Deficiencias)
   }),
   components: {
-    FormHeader: FormHeader,
-    RowSwitch: RowSwitch
+    RowSwitch: () => import("@/components/RowSwitch"),
+    FormHeader: () => import("@/components/forms/FormHeader")
   }
 };
 </script>
