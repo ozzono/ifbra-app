16d2832a998aecb5ec429bf2ad15fdd6beab31ce
Hugo Virgílio <huvirgilio@gmail.com>
2020-08-12 13:16:41 -0300

    Fixed element vertical alignment

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
-    <Evaluation />
-    <Report />
-    <Form1 />
-    <Form2 />
-    <Form3 />
+    <!-- <Evaluation /> -->
+    <!-- <Report /> -->
+    <!-- <Form1 /> -->
+    <!-- <Form2 /> -->
+    <!-- <Form3 /> -->
+    <!-- <Align /> -->
     <Form4 />
   </div>
 </template>
@@ -12,11 +13,12 @@
 <script>
 export default {
   components: {
-    Evaluation: () => import("@/components/Evaluation"),
-    Report: () => import("@/components/Report"),
-    Form1: () => import("@/components/forms/Form1"),
-    Form2: () => import("@/components/forms/Form2"),
-    Form3: () => import("@/components/forms/Form3"),
+    // Evaluation: () => import("@/components/Evaluation"),
+    // Report: () => import("@/components/Report"),
+    // Form1: () => import("@/components/forms/Form1"),
+    // Form2: () => import("@/components/forms/Form2"),
+    // Form3: () => import("@/components/forms/Form3"),
+    // Align: () => import("@/components/test/Align"),
     Form4: () => import("@/components/forms/Form4")
   }
 };
--- src/components/forms/Form4.vue
+++ src/components/forms/Form4.vue
@@ -14,41 +14,28 @@
         </v-row>
         <div v-for="(deficiecia, i) in Deficiencias" :key="i">
           <v-divider v-if="i > 0" class="hidden-md-and-up" :inset="true" />
-          <v-row class="align-center">
+          <v-row class="align-center d-flex">
             <v-col class="text-center" md="2" cols="12">
               {{ deficiecia.Desc }}
             </v-col>
             <v-col cols="10">
-              <v-row>
+              <v-row class="align-center d-flex" flat tile>
                 <v-col cols="12" md="4">
-                  <v-row class="align-center">
-                    <v-col>
-                      <RowSwitch :innerLabel="deficiecia.Obs" />
-                    </v-col>
-                  </v-row>
+                  <RowSwitch :innerLabel="deficiecia.Obs" />
                 </v-col>
                 <v-col cols="12" md="4">
-                  <v-row class="align-center">
-                    <v-col cols="12">
-                      <RowSwitch
-                        :innerLabel="
-                          `Houve pontuação 25 ou 50 em alguma atividade dos domínios ${deficiecia.Dominios}`
-                        "
-                      />
-                    </v-col>
-                    <v-col> </v-col>
-                  </v-row>
+                  <RowSwitch
+                    :innerLabel="
+                      `Houve pontuação 25 ou 50 em alguma atividade dos domínios ${deficiecia.Dominios}`
+                    "
+                  />
                 </v-col>
                 <v-col cols="12" md="4">
-                  <v-row class="align-center">
-                    <v-col cols="12">
-                      <RowSwitch
-                        :innerLabel="
-                          `Houve pontuação 75 em todas as atividade dos domínios ${deficiecia.Dominios}`
-                        "
-                      />
-                    </v-col>
-                  </v-row>
+                  <RowSwitch
+                    :innerLabel="
+                      `Houve pontuação 75 em todas as atividade dos domínios ${deficiecia.Dominios}`
+                    "
+                  />
                 </v-col>
               </v-row>
             </v-col>
