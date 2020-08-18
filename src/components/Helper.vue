0c46435d4575d077cb2ef8e6a881cec8dc69bd2b
Hugo Virgílio <huvirgilio@gmail.com>
2020-08-18 14:57:25 -0300

    Implement v0 for Helper page

--- src/components/Helper.vue
+++ src/components/Helper.vue
@@ -1,9 +1,67 @@
 <template>
-  <div>Helper to be done</div>
+  <div>
+    <h3 class="text-center">Legenda</h3>
+    <v-container>
+      <v-card>
+        <v-list header two-line flat>
+          <v-header class="header font-weight-medium">
+            Detalhamento de pontuação do INSS
+          </v-header>
+          <v-list-item-group v-for="(item, i) in INSSDesc" :key="i">
+            <v-list-item>
+              <v-list-item-action class="desc justify-center">
+                {{ item.MainCol }}
+              </v-list-item-action>
+              <v-list-item-content>
+                {{ item.DescCol }}
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-card>
+    </v-container>
+    <v-container>
+      <v-card>
+        <v-list header two-line flat>
+          <v-header class="header font-weight-medium">
+            Descrição das Barreiras
+          </v-header>
+          <v-list-item-group v-for="(item, i) in BarreirasDesc" :key="i">
+            <v-list-item>
+              <v-list-item-action class="desc justify-center">
+                {{ item.MainCol }}
+              </v-list-item-action>
+
+              <v-list-item-content>
+                {{ item.DescCol }}
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-card>
+    </v-container>
+  </div>
 </template>
 
 <script>
-export default {};
+import INSSDesc from "@/assets/json/inss.json";
+import BarreirasDesc from "@/assets/json/barreiras.json";
+export default {
+  data: () => ({
+    INSSDesc: Object.values(INSSDesc),
+    BarreirasDesc: Object.values(BarreirasDesc)
+  })
+};
 </script>
 
-<style></style>
+<style scoped>
+.header {
+  padding-left: 1rem;
+}
+.desc {
+  min-width: 3rem;
+}
+h3 {
+  padding: 1rem;
+}
+</style>
