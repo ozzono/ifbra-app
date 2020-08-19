9a2f0cf4c747b9bb7515d4506e733b5aa04c67df
Hugo Virgílio <huvirgilio@gmail.com>
2020-08-18 23:46:52 -0300

    Using simplified helper while developing and debugging

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,9 +4,8 @@
     <!-- <Report /> -->
     <!-- <Form1 /> -->
     <!-- <Form2 /> -->
-    <!-- <Form3 /> -->
-    <!-- <Align /> -->
-    <Form4 />
+    <Form3 />
+    <!-- <Form4 /> -->
   </div>
 </template>
 
@@ -17,9 +16,8 @@ export default {
     // Report: () => import("@/components/Report"),
     // Form1: () => import("@/components/forms/Form1"),
     // Form2: () => import("@/components/forms/Form2"),
-    // Form3: () => import("@/components/forms/Form3"),
-    // Align: () => import("@/components/test/Align"),
-    Form4: () => import("@/components/forms/Form4")
+    Form3: () => import("@/components/forms/Form3"),
+    // Form4: () => import("@/components/forms/Form4")
   }
 };
 </script>
