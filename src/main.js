f5f4dc81899224f3530eba115af1237ee6128dd5
Hugo Virgilio <huvirgilio@gmail.com>
2020-10-07 18:42:30 -0300

    Removing a few more

--- src/main.js
+++ src/main.js
@@ -7,17 +7,8 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@mdi/font/css/materialdesignicons.css";
-import VueHtmlToPaper from "vue-html-to-paper";
 Vue.config.productionTip = false;
 Vue.prototype.$eventHub = new Vue();
-Vue.use(VueHtmlToPaper, {
-  name: "_blank",
-  specs: ["fullscreen=yes", "titlebar=yes", "scrollbars=yes"],
-  styles: [
-    "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css",
-    "https://unpkg.com/kidlat-css/css/kidlat.css"
-  ]
-});
 
 new Vue({
   router,
