bc56fdfd0263b32c1603de995c953d614aef2095
Hugo Virgilio <huvirgilio@gmail.com>
2020-10-07 22:26:39 -0300

    Removed popper; jquery; core-js; mutationobserver; bootstrap; html2pdf; jsonloader; vue-html-to-pdf

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import "@babel/polyfill";
-import "mutationobserver-shim";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
@@ -7,17 +6,8 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@mdi/font/css/materialdesignicons.css";
-import VueHtmlToPaper from "vue-html-to-paper";
 Vue.config.productionTip = false;
 Vue.prototype.$eventHub = new Vue();
-Vue.use(VueHtmlToPaper, {
-  name: "_blank",
-  specs: ["fullscreen=yes", "titlebar=yes", "scrollbars=yes"],
-  styles: [
-    "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css",
-    "https://unpkg.com/kidlat-css/css/kidlat.css"
-  ]
-});
 
 new Vue({
   router,
